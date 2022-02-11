c5b7477abf16fedcd0e820d4c1dfd6b56436f2b3
Ben Girardet <ben@platform5.ch>
2022-02-11 22:48:28 +0100

    Ex 1 to 4 (without ellipsis)

--- src/components/PhotoGridItem/PhotoGridItem.js
+++ src/components/PhotoGridItem/PhotoGridItem.js
@@ -2,10 +2,36 @@ import React from 'react';
 import styled from 'styled-components/macro';
 
 const PhotoGridItem = ({ id, src, alt, tags }) => {
+
+  const baseSrc = src.replace('.jpg', '');
+
+  const avifSet = [
+    `${baseSrc}.avif 1x`,
+    `${baseSrc}@2x.avif 2x`,
+    `${baseSrc}@3x.avif 3x`,
+  ].join(', ');
+  const jpgSet = [
+    `${baseSrc}.jpg 1x`,
+    `${baseSrc}@2x.jpg 2x`,
+    `${baseSrc}@3x.jpg 3x`,
+  ].join(', ');
+
+
   return (
     <article>
       <Anchor href={`/photos/${id}`}>
-        <Image src={src} />
+
+        <picture>
+          <source
+            type="image/avif"
+            srcSet={avifSet}
+          ></source>
+          <source
+            type="image/jpg"
+            srcSet={jpgSet}
+          ></source>
+          <Image src={src} alt={alt} />
+        </picture>
       </Anchor>
       <Tags>
         {tags.map((tag) => (
@@ -28,20 +54,26 @@ const Image = styled.img`
   height: 300px;
   border-radius: 2px;
   margin-bottom: 8px;
+  object-fit: cover;
 `;
 
 const Tags = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 8px;
-`;
+  white-space: nowrap;
+  overflow: hidden;
+  `;
 
 const Tag = styled.li`
+  display: inline-block;
   padding: 4px 8px;
   background: var(--color-gray-300);
   font-size: 0.875rem;
   font-weight: 475;
   color: var(--color-gray-800);
+  margin-left: 8px;
+  
+  &:first-of-type {
+    margin-left: 0;
+  }
 `;
 
 export default PhotoGridItem;
